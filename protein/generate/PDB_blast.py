fed662905e959e94e32390f7d933128fbda1edf8
matteoferla <matteo.ferla@gmail.com>
2019-03-20 18:00:39 +0000

    :wrench: I hate windows so much. Why cant I have ubuntu?

--- protein/generate/PDB_blast.py
+++ protein/generate/PDB_blast.py
@@ -1,4 +1,4 @@
-__docs__="""
+__docs__ = """
 This part requires NCBI blast tools!
 """
 import json
@@ -6,6 +6,8 @@ import os
 import tarfile
 from ..settings_handler import global_settings
 from Bio.Blast import NCBIXML
+from Bio.Blast.Applications import NcbiblastpCommandline ## because I am using a ducking window piece of crap
+
 
 class Blaster:
     """
@@ -21,31 +23,30 @@ class Blaster:
 
     @classmethod
     def make_fastas(cls):
-        #load
-        genes={}
-        name='error'
+        # load
+        genes = {}
+        name = 'error'
         for line in open(os.path.join(global_settings.temp_folder, 'human.fa')):
             if '>' in line:
                 name = line[1:].rstrip()
-                genes[name]=''
+                genes[name] = ''
             else:
-                genes[name]+=line.rstrip()
-        #dump
+                genes[name] += line.rstrip()
+        # dump
         folder = os.path.join(global_settings.temp_folder, 'fasta')
         os.mkdir(folder)
         for acc in genes:
-            with open(os.path.join(folder,acc+'.fa'),'w') as w:
-                w.write('>'+acc+'\n'+genes[acc]+'\n')
+            with open(os.path.join(folder, acc + '.fa'), 'w') as w:
+                w.write('>' + acc + '\n' + genes[acc] + '\n')
         return cls
 
     @classmethod
     def pdb_blaster(cls):
-        return cls.full_blaster('blastpdb','pdbaa')
+        return cls.full_blaster('blastpdb', 'pdbaa')
 
     @classmethod
     def self_blaster(cls):
         return cls.full_blaster('blastself', 'human')
-
 
     @classmethod
     def full_blaster(cls, outfolder_name, db):
@@ -54,15 +55,16 @@ class Blaster:
         :return:
         """
         outfolder = os.path.join(global_settings.temp_folder, outfolder_name)
-        os.mkdir(outfolder)
+        if not os.path.exists(outfolder):
+            os.mkdir(outfolder)
         infolder = os.path.join(global_settings.temp_folder, 'fasta')
         if not os.path.exists(infolder):
             cls.make_fastas()
         for infile in os.listdir(infolder):
-            outfile = infile.replace('.fa','.xml')
-            os.system('blastp -query {infile} -db {db} -outfmt 5 -num_threads 6 > {outfile}'.format(db=db,
-                                                                                                    infile=os.path.join(infolder,infile),
-                                                                                                     outfile=os.path.join(outfolder,outfile)))
+            outfile = infile.replace('.fa', '.xml')
+            if global_settings.verbose:
+                print(infile)
+            NcbiblastpCommandline(query=os.path.join(infolder, infile), db=db, evalue=0.001, outfmt = 5, out = os.path.join(outfolder, outfile))()
         return cls
 
     @staticmethod
@@ -72,42 +74,39 @@ class Blaster:
         :param todo: todo is a set of ids that may have failed.
         :return:
         """
-        dex = json.load(open('human_prot_seqdex.json','r'))
+        dex = json.load(open('human_prot_seqdex.json', 'r'))
         for k in todo:
-            file='blastpdb/'+k+'.fa'
-            open(file,'w').write('>{i}\n{s}\n\n'.format(s=dex[k],i=k))
-            os.system('blastp -query {infile} -db pdbaa -outfmt 5 -num_threads 6 > {outfile}'.format(infile=file, outfile=file.replace('.fa','_blastPDB.xml')))
+            file = 'blastpdb/' + k + '.fa'
+            open(file, 'w').write('>{i}\n{s}\n\n'.format(s=dex[k], i=k))
+            os.system('blastp -query {infile} -db pdbaa -outfmt 5 -num_threads 6 > {outfile}'.format(infile=file, outfile=file.replace('.fa', '_blastPDB.xml')))
 
     @staticmethod
     def parse(folder):
-        for file in os.listdir(os.path.join(global_settings.temp_folder,folder)):
+        for file in os.listdir(os.path.join(global_settings.temp_folder, folder)):
             if '_blast.xml' in file:
                 print(file)
                 try:
-                    blast_record = NCBIXML.read(open(os.path.join(global_settings.temp_folder,folder,file)))
+                    blast_record = NCBIXML.read(open(os.path.join(global_settings.temp_folder, folder, file)))
                     matches = []
                     for align in blast_record.alignments:
                         print(align.title)
                         for hsp in align.hsps:
                             if hsp.score > 100:
-                                d = {'match': align.title[0:50],
-                                     'match_score': hsp.score,
-                                     'match_start': hsp.query_start,
-                                     'match_length': hsp.align_length,
-                                     'match_identity': hsp.identities / hsp.align_length}
-                                d['formatted'] = {'x': hsp.query_start,
-                                                  'y': hsp.align_length + hsp.query_start,
-                                                  'description': align.title[0:20],
-                                                  'id': 'blastpdb_'}
+                                d = {'x': hsp.query_start,
+                                     'y': hsp.align_length + hsp.query_start,
+                                     'description': align.title[0:20],
+                                     'id': 'blastpdb_',
+                                     'original': {'match': align.title[0:50],
+                                                  'match_score': hsp.score,
+                                                  'match_start': hsp.query_start,
+                                                  'match_length': hsp.align_length,
+                                                  'match_identity': hsp.identities / hsp.align_length}}
                                 matches.append(d)
                 except ValueError as err:
-                    print('Value error: '+str(err)) ##why art thou so empty?
+                    print('Value error: ' + str(err))  ##why art thou so empty?
 
     @staticmethod
     def _test_describe():
         blast_record = NCBIXML.read(open(os.path.join(global_settings.temp_folder, 'blastpdb', 'S438966_blast.xml')))
         print(blast_record.alignments)
         print(blast_record.alignments[0].title)
-
-
-
