f50c565773eaa99fc87eace1da9b2d34e48f3106
matteoferla <matteo.ferla@gmail.com>
2020-02-11 14:39:05 +0000

    :clown_face: Adding mode to make it run without pyrosetta or PyMOL

--- michelanglo_protein/analyse/__init__.py
+++ michelanglo_protein/analyse/__init__.py
@@ -1,2 +1,10 @@
-from .Pymol_StructureAnalyser import StructureAnalyser
-from .pyrosetta_modifier import Mutator
+try:
+    from .Pymol_StructureAnalyser import StructureAnalyser
+    from .pyrosetta_modifier import Mutator
+except ModuleNotFoundError as err:
+    import warnings
+    warnings.warn(f'**{err.__class__.__name__}** {err}.')
+    print('If you are not running Michelanglo in full this is fine.')
+    print('Without pyrosetta VENUS will not complete. Without PyMOL editing of coordinates will not work.')
+    StructureAnalyser = None
+    Mutator = None
